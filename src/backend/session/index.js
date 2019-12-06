53155851248d8e2ac4820ea256a8369c557e909d
Forrest <forrest.ching@gmail.com>
2019-12-05 17:10:43 -1000

    issue #6 complete

--- src/backend/session/index.js
+++ src/backend/session/index.js
@@ -1,7 +1,9 @@
 import AuthUserContext from './context';
 import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
 
 export {
 	AuthUserContext,
-	withAuthentication
+	withAuthentication,
+	withAuthorization
 };
--- src/frontend/Account.js
+++ src/frontend/Account.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
+import { withAuthorization, AuthUserContext } from '../backend/session';
+
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
